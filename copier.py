fa461af21d61f6ac6874bf99bbb0f4ec6f40ff74
David Barchiesi <david@barchie.si>
2018-11-22 17:44:47 +0100

    Add region limit.

--- copier.py
+++ copier.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 
 
 def copy_zipped(rompath, rom_entry, output_dir):
-    final_filename = os.path.splitext(rom_entry['filename'])[0]+'.zip'
+    final_filename = os.path.splitext(rom_entry['rom_filename'])[0]+'.zip'
     finalpath = os.path.join(output_dir, final_filename)
 
     if is_zipfile(rompath):
@@ -33,17 +33,17 @@ def compute_md5(rompath):
     return md5sum.hexdigest()
 
 
-def handle_rompath(rompath, known_roms, output_dir):
+def handle_rompath(rompath, wanted_roms, output_dir):
     try:
         md5sum = compute_md5(rompath)
-        rom_entry = known_roms[md5sum.lower()]
+        rom_entry = wanted_roms[md5sum.lower()]
 
         if rom_entry['seen']:
-            print(f'INFO: Duplicate rom "{rom_entry["filename"]}" at "{rompath}" with md5 "{md5sum}"')
+            print(f'INFO: Duplicate rom "{rom_entry["rom_filename"]}" at "{rompath}" with md5 "{md5sum}"')
             return
 
         copy_zipped(rompath, rom_entry, output_dir)
-        print(f'INFO: Known rom "{rom_entry["filename"]}" at "{rompath}" with md5 "{md5sum}"')
+        print(f'INFO: Known rom "{rom_entry["rom_filename"]}" at "{rompath}" with md5 "{md5sum}"')
 
     except PermissionError as e:
         print(f'INFO: Failed reading rom at "{rompath}": {e}')
@@ -62,33 +62,69 @@ def iterate_roms(input_dirs):
             yield input_file.path
 
 
-def build_known_roms(filename):
-    known_roms = {}
+def build_all_roms(known_games):
+    wanted_roms = {}
+    for game_clones in known_games.values():
+        for game in game_clones:
+            wanted_roms[game['rom_md5']] = game
+    return wanted_roms
+
+
+def build_wanted_roms(known_games, region_limit):
+    wanted_roms = {}
+    for game_clones in known_games.values():
+        found = False
+        for region in region_limit:
+            for game in game_clones:
+                if 'region' not in game:
+                    break
+                if game['region'] == region:
+                    wanted_roms[game['rom_md5']] = game
+                    found = True
+                    break
+
+            if found:
+                break
+
+    return wanted_roms
+
+
+def build_known_games(filename):
+    known_games = {}
     tree = ET.parse(filename)
     root = tree.getroot()
-    for game_tag in root.findall('.//game'):
-        rom_tag = game_tag.find('.//rom')
-        rom_md5 = rom_tag.attrib['md5']
-        rom_filename = rom_tag.attrib['name']
-
-        if not rom_tag and not rom_filename:
-            print(f'Error for game, missing one of md5: "{rom_md5}" or filename: "{rom_filename}"')
-            continue
-
-        if rom_md5 in known_roms:
-            print(f'Error for game, already existing md5: "{rom_md5}" for filename: "{rom_filename}"')
-            continue
-
-        known_roms[rom_md5.lower()] = {
-            'filename': rom_filename,
+    for game_elem in root.findall('.//game'):
+        game = {
+            'name': game_elem.attrib['name'],
             'seen': False
         }
+        if 'cloneof' in game_elem.attrib:
+            game_key = game_elem.attrib['cloneof']
+        else:
+            game_key = game['name']
 
-    return known_roms
+        game_release_elem = game_elem.find('.//release')
+        if game_release_elem != None:
+            game['region'] = game_release_elem.attrib['region']
+
+        rom_elem = game_elem.find('.//rom')
+        game['rom_md5'] = rom_elem.attrib['md5']
+        game['rom_filename'] = rom_elem.attrib['name']
+
+        game_clones = known_games.get(game_key, [])
+        game_clones.append(game)
+        known_games[game_key] = game_clones
+
+    return known_games
 
 
-def process(input_dirs, output_dir, dat_file):
-    known_roms = build_known_roms(dat_file)
+def process(input_dirs, output_dir, dat_file, region_limit):
+    known_games = build_known_games(dat_file)
+
+    if region_limit:
+        wanted_roms = build_wanted_roms(known_games, region_limit)
+    else:
+        wanted_roms = build_all_roms(known_games)
 
     for rompath in iterate_roms(input_dirs):
-        handle_rompath(rompath, known_roms, output_dir)
+        handle_rompath(rompath, wanted_roms, output_dir)
--- main.py
+++ main.py
@@ -7,6 +7,39 @@ import argparse
 
 from copier import process
 
+KNOWN_REGIONS = [
+'ASI',
+'AUS',
+'BRA',
+'CAN',
+'CHN',
+'EUR',
+'FRA',
+'GER',
+'HOL',
+'ITA',
+'JPN',
+'KOR',
+'SPA',
+'SWE',
+'USA',
+]
+
+
+def clean_region_limit(options):
+    if options.region_limit is not None:
+        raw_region_limit = options.region_limit
+        options.region_limit = []
+        for region in raw_region_limit.split(','):
+            if region in KNOWN_REGIONS:
+                options.region_limit.append(region)
+            else:
+                print(f'main.py: error: argument -l/--region_limit: invalid value. Must be comma separated list of:')
+                for region in KNOWN_REGIONS:
+                    print(f'main.py: error: argument -l/--region_limit:     {region}')
+                sys.exit(2)
+
+
 def validate_dirs(input_dirs, output_dir):
     check_dirs = input_dirs + [output_dir]
     for directory in check_dirs:
@@ -21,6 +54,7 @@ def validate_dirs(input_dirs, output_dir):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Extract into output directory all valid roms according to No-intro dat file.')
+    parser.add_argument('-l', '--region_limit', action='store', dest='region_limit', required=False, help='Limit to comma separated region list', metavar='REGION_LIMIT')
     parser.add_argument('-i', '--input_dir', action='append', dest='input_dirs', required=True, help='One or more input directories', metavar='INPUT_DIR')
     parser.add_argument('-o', '--output_dir', action='store', required=True, help='Output directory (must be empty)', metavar='OUTPUT_DIR')
     parser.add_argument('-d', '--dat', action='store', required=True, help='No-intro dat file', metavar='DAT_FILE')
@@ -30,6 +64,7 @@ def parse_args():
 
 if __name__ == '__main__':
     options = parse_args()
+    clean_region_limit(options)
     validate_dirs(options.input_dirs, options.output_dir)
 
-    process(options.input_dirs, options.output_dir, options.dat)
+    process(options.input_dirs, options.output_dir, options.dat, options.region_limit)
